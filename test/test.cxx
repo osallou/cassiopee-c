1bb40e015d8aa733b5ff0777f23e3ccf5fdde135
Olivier Sallou <olivier.sallou@irisa.fr>
2014-01-03 16:42:51 +0100

    add xml generation

--- test/test.cxx
+++ test/test.cxx
@@ -1,6 +1,9 @@
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/TestResult.h>
 
 #include "test.h"
 
@@ -27,12 +30,27 @@ void CassiopeeTest::testExample()
 
 int main(int argc, char* argv[])
 {
+  CppUnit::TestResultCollector result;
+  CppUnit::TestResult controller;
+
+  controller.addListener( &result );
+
   CppUnit::TextUi::TestRunner runner;
   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
   runner.addTest( registry.makeTest() );
   // Run the tests.
-  bool wasSucessful = runner.run();
+  runner.run(controller);
+
+  // Print test in a compiler compatible format.
+  CppUnit::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+  outputter.write();
+ 
+  // Uncomment this for XML output
+  std::ofstream file( "cppunit-report.xml" );
+  CppUnit::XmlOutputter xml( &result, file );
+  xml.write();
+  file.close();
 
   // Return error code 1 if the one of test failed.
-  return wasSucessful ? 0 : 1;
+  return result.wasSuccessful() ? 0 : 1;
 }
